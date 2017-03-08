3fb797886769014d91a756fcef5f6ca909fcf602
Max A. Kiselev <kiselevma@gmail.com>
2017-03-09 01:06:15 +0300

    Added sample configuration files.

---
+++ Sample.New-DarBackup.Config.ps1
@@ -0,0 +1,20 @@
+$local:DefaultTarget = @(
+    @{
+        Target = 't-profile-main';
+        Name = 'profile-{0:yyyy-MM-dd}-{1}';
+        NameParser = '^profile-(?<date>\d{4}-\d{2}-\d{2})-(?<kind>full|diff)$';
+    },
+    @{
+        Target = 't-profile-private';
+        Name = 'profile-{0:yyyy-MM-dd}-{1}-private';
+        NameParser = '^profile-(?<date>\d{4}-\d{2}-\d{2})-(?<kind>full|diff)-private$';
+    }
+);
+$local:DefaultDarConfig = Join-Path $PSScriptRoot 'New-DarBackup.Config.dcf';
+$local:DefaultBackupPath = '\\NAS\Backup\dar';
+$local:DefaultLogPath = '';
+$local:DefaultDarPath = 'C:\Program Files (x86)\dar\dar.exe';
+$local:DefaultFullSuffix = 'full';
+$local:DefaultDiffSuffix = 'diff';
+$local:MaxDiffBackupsSinceLastFull = 31;
+$local:MaxDiffBackupSizeToFullBackupSizeRatio = 0.10;
